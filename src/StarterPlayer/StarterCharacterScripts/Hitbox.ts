4a002d59368ce3ab03546b0dd2dfdf84fa215cbb
BobJoeGaming <134542094+BobJoeGaming@users.noreply.github.com>
2024-11-22 11:28:31 -0600

    Update Hitbox.ts

--- src/StarterPlayer/StarterCharacterScripts/Hitbox.ts
+++ src/StarterPlayer/StarterCharacterScripts/Hitbox.ts
@@ -1,6 +1,6 @@
 import { BacktrackableAimHistory } from "ServerStorage/History/BacktrackableAimHistory";
 import { BacktrackableHitboxCFrame } from "ServerStorage/History/BacktrackableHitboxCFrame";
 
-const FRAME_COUNT = 30;
+const FRAME_COUNT = 60;
 
 export let Hitbox =  new BacktrackableHitboxCFrame(<Model>(script.Parent), FRAME_COUNT)
